481a556dfca9b36b21b9c2370a76d918dc115d8d
Chiba <ryotai2677@gmail.com>
2024-03-06 14:54:34 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Green-base`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
